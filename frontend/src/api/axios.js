e60840e0254b29c9032935e2c9b0fd9a8a480ad6
Tarun Verma <tarunverma@132004@gmail.com>
2025-09-18 21:02:28 +0530

    adding url in backend to connect frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://blixora-umber.vercel.app/api' });
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
